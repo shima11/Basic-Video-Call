ad038b31856406883fe547ea94e2630232506bb5
Shima <shima.jin@icloud.com>
2021-07-07 17:47:54 +0900

    update

--- Group-Video/OpenVideoCall-iOS/OpenVideoCall/KeyCenter.swift
+++ Group-Video/OpenVideoCall-iOS/OpenVideoCall/KeyCenter.swift
@@ -8,7 +8,7 @@
 
 
 struct KeyCenter {
-    static let AppId: String = <#Your App Id#>
+    static let AppId: String = "01ffafb3863b4fec95f75f148100dc0d"
     // assign token to nil if you have not enabled app certificate
-    static var Token: String? = <#Temp Access Token#>
+    static var Token: String? = "00601ffafb3863b4fec95f75f148100dc0dIAD/OaXfpTeQb3VYoRBa+4IY+UBI65+Tp57S3klyqypshMt3QIEAAAAAEAAltn4OuHvmYAEAAQC3e+Zg"
 }
